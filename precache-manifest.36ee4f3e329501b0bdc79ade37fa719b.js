3384c7954f0f0684a488a6f578fa90b478288cd0
euloong <euloong@hotmail.com>
2020-08-28 19:55:30 +1000

    Updates

---
+++ precache-manifest.36ee4f3e329501b0bdc79ade37fa719b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8898a5dffcc230f83109b49284db75b0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c26eb475bd1a0ed8e9d",
+    "url": "/monsters-rolodex/static/css/main.70a45427.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c26eb475bd1a0ed8e9d",
+    "url": "/monsters-rolodex/static/js/main.737e9dae.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
